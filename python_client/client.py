5e9cc91efa66f2417fcb49f9f4ce12f9f9d76d22
Santiago Zubieta <santiago.zubieta@gmail.com>
2016-10-11 20:32:30 -0500

    Python Impact

---
+++ python_client/client.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import grpc
+import helloworld_pb2
+
+def run():
+  # The address of the GRPC Server.
+  address = '52.34.52.17:50001'
+  # Create a connection to that address.
+  channel = grpc.insecure_channel(address)
+  # Create a GRPC client from that connection.
+  client = helloworld_pb2.SearchServiceStub(channel)
+  # Make a GRPC call to the server.
+  response = client.Search(helloworld_pb2.SearchRequest(query='Medellin DevOps'))
+  # Print the server's response
+  print("GRPC Response: " + response.message)
+
+if __name__ == '__main__':
+  run()
+
+
